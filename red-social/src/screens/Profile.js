c0cc0693bfcd05b0a62d46b27b7165d6491abf39
tomipoitevin2103 <tgarciapoitevin@udesa.edu.ar>
2022-06-20 12:22:11 -0300

    ahora se pueden ver nuestros posteos en el perfil, como en instagram

--- red-social/src/screens/Profile.js
+++ red-social/src/screens/Profile.js
@@ -1,28 +1,71 @@
 import React, {Component} from "react"
-import {Text, TouchableOpacity,View} from 'react-native';
-import {auth} from "../firebase/config" 
+import {Text, 
+		TouchableOpacity,
+		View, 
+		FlatList, 
+		StyleSheet} from 'react-native';
+import {auth, db} from "../firebase/config" 
+import Post from './Post'
+
 
 class Profile extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
 			email:'',
-			password:''
+			password:'',
+			posts: [],
+			user:{},
 		}
 	}
+
+componentDidMount(){ 
+	db.collection('posts').where("owner", "==", auth.currentUser.email).onSnapshot(
+		docs => { 
+			let posts = []; 
+			docs.forEach( oneDoc => {
+				posts.push({
+					id: oneDoc.id, 
+					data: oneDoc.data()
+				})
+			})
+
+			this.setState({
+				posts: posts
+		})
+		}
+	)
+}
+
+
 	render(){
 		return (
-			<View>
+			<View style={styles.styleProfile}>
 				<Text>{auth.currentUser.email}</Text>
 		                <Text>Since: {auth.currentUser.metadata.creationTime}</Text>
 				<Text>Último acceso: {auth.currentUser.metadata.lastSignInTime}</Text>
 				<TouchableOpacity onPress= {()=> this.props.route.params.logout()}>
 					<Text>LogOut</Text>
-				</TouchableOpacity>
+				</TouchableOpacity> 
+				<View >
+				<FlatList 
+					data={this.state.posts}
+					keyExtractor={post => post.id.toString()}
+					renderItem = { ({item})  => <Post dataPost={item} 
+					{...this.props} />}
+				
+				/>
+				</View>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+    styleProfile:{
+        flex: 1
+    }
+})
 
 export default Profile;
 
